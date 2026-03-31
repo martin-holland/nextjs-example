35d8033a6ac52ee9c6da066cd48b8503d1b47371
Martin Holland <martin.holland@free.fi>
2026-03-31 09:19:03 +0300

    updated route

--- app/api/route.ts
+++ app/api/route.ts
@@ -7,20 +7,19 @@
 
 // API call to supabase using REST API
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-
-// Direct API call to supabase using fetch
-const response = await fetch(`${supabaseUrl}/rest/v1/products`, {
-  headers: {
-    Authorization: `Bearer ${supabaseAnonKey}`,
-    apikey: supabaseAnonKey,
-    "Content-Type": "application/json",
-  },
-});
-
-const data = await response.json();
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
 export async function GET() {
+  // Direct API call to supabase using fetch
+  const response = await fetch(`${supabaseUrl}/rest/v1/products`, {
+    headers: {
+      Authorization: `Bearer ${supabaseAnonKey}`,
+      apikey: supabaseAnonKey,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
   return Response.json(data);
 }
